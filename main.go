5843bdf1d1c7f3bf27cb2fd6b55ab8a1f53f07e9
Dan Kirkwood <daniel_kirkwood@cable.comcast.com>
2015-11-29 12:00:53 -0700

    snapshot

--- main.go
+++ main.go
@@ -18,17 +18,49 @@ func init() {
 	relNeighbors := make(Grid, len(r)^2-1)
 	for _, x := range r {
 		for _, y := range r {
-			relNeighbors[Cell{x, y}] = struct{}{}
+			if x != 0 || y != 0 {
+				relNeighbors[Cell{x, y}] = struct{}{}
+			}
 		}
 	}
-	fmt.Printf("neighborgrid is %#v\n", relNeighbors)
+}
+
+func (g Grid) countNeighbors() neighborCount {
+	// for each cell, increment neighbor count of each of its neighbors
+	ncount := make(neighborCount, len(g)*8)
+	for c, _ := range g {
+		for n, _ := range relNeighbors {
+			nc := Cell{c.x + n.x, c.y + n.y}
+			ncount[nc]++
+		}
+	}
+	return ncount
+}
+
+func (g Grid) nextGen() {
+	nc := g.countNeighbors()
+	// for each live cell, if not enough or too many neighbors, kill it
+	for c := range g {
+		if nc[c] < 2 || nc[c] > 3 {
+			delete(g, c)
+		}
+		delete(nc, c)
+	}
+	// only non-live still in neighbor count -- for each with 3 neighbors emerges
+	for deadcell, n := range nc {
+		if n == 3 {
+			g[deadcell] = struct{}{}
+		}
+	}
 }
 
 func main() {
 	g := make(Grid, 5)
 
 	c := Cell{5, 5}
+
 	g[c] = struct{}{}
+	g.nextGen()
 
 	fmt.Printf("grid is %#v\n", g)
 }
